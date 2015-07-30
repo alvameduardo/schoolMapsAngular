d4ec8c3aae6565634486a6c2c30de15716b12fcf
Eduardo Alva <Eduardo@MacBook-Pro-de-Eduardo.local>
2015-07-30 16:54:37 -0500

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name identicoleAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the identicoleAngularApp
+ */
+angular.module('identicoleAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name identicoleAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the identicoleAngularApp
+ */
+angular.module('identicoleAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
